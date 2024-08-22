9ef5b70c3bd20690cf2523395b3aed189b97ee02
Bernardo Biesseck <biesseck@gmail.com>
2024-08-22 17:29:27 -0300

    dcface/src/scripts/train_with_BFM_consistency_constraints.sh: add parameters BFM_ID_LAMBDA, BFM_EXP_LAMBDA, and BFM_POSE_LAMBDA; remove parameter BFM_LOSS_LAMBDA

--- dcface/src/scripts/train_with_BFM_consistency_constraints.sh
+++ dcface/src/scripts/train_with_BFM_consistency_constraints.sh
@@ -8,8 +8,11 @@ BATCH_GPU=16       # duo     (16.0GB)
 # BATCH_GPU=32     # duo     ()
 # BATCH_GPU=256    # daugman (40.0GB)
 
-DATA_MODULE=casia_webface                 # original
-# DATA_MODULE=casia_webface_imgs_crops    # Bernardo
+DATA_MODULE=casia_webface                          # original (whole dataset, 10k classes)
+# DATA_MODULE=casia_webface_1000CLASS_SEQUENTIAL   # Bernardo
+# DATA_MODULE=casia_webface_2000CLASS_SEQUENTIAL   # Bernardo
+# DATA_MODULE=casia_webface_5000CLASS_SEQUENTIAL   # Bernardo
+# DATA_MODULE=casia_webface_imgs_crops             # Bernardo
 
 # ThreeDMM_LOSS_LAMBDA=0.001
 # ThreeDMM_LOSS_LAMBDA=0.005
@@ -17,8 +20,24 @@ DATA_MODULE=casia_webface                 # original
 
 # BFM_LOSS_LAMBDA=0.001
 # BFM_LOSS_LAMBDA=0.005
-BFM_LOSS_LAMBDA=0.01
+# BFM_LOSS_LAMBDA=0.01
 # BFM_LOSS_LAMBDA=0.05                    # same as 'identity_consistency_loss_lambda'
+
+
+
+BFM_ID_LAMBDA=0.0
+# BFM_ID_LAMBDA=0.01
+# BFM_ID_LAMBDA=0.05
+
+# BFM_EXP_LAMBDA=0.0
+# BFM_EXP_LAMBDA=0.01
+BFM_EXP_LAMBDA=0.05
+
+# BFM_POSE_LAMBDA=0.0
+# BFM_POSE_LAMBDA=0.01
+BFM_POSE_LAMBDA=0.05
+
+
 
 # DATALOADER_DETERMINISTIC=false          # default (using data augmentation)
 DATALOADER_DETERMINISTIC=true             # Bernardo (no data augmentation)
@@ -50,7 +69,9 @@ python src/train_with_BFM_consistency_constraints.py \
         losses.identity_consistency_loss_center_source=id_image \
         losses.identity_consistency_loss_time_cut=0.0 \
         losses.identity_consistency_loss_weight_start_bias=0.0 \
-        losses.bfm_consistency_loss_lambda=$BFM_LOSS_LAMBDA \
+        losses.bfm_consistency_id_lambda=$BFM_ID_LAMBDA \
+        losses.bfm_consistency_exp_lambda=$BFM_EXP_LAMBDA \
+        losses.bfm_consistency_pose_lambda=$BFM_POSE_LAMBDA \
         losses.spatial_consistency_loss_lambda=0.0 \
         losses.latent_mixup_loss_lambda=0.0 \
         losses.face_contour_loss_lambda=0.0 \
